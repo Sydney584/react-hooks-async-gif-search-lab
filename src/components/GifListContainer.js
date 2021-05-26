fac1d3741cd7b2cd655405cf9af107743abaaad9
sydney <sydneychase5841@gmail.com>
2021-05-25 20:30:24 -0400

    Assignment Updated

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import GifSearch from "./GifSearch";
+import GifList from "./GifList";
+
+function GifListContainer() {
+    
+    const [gifs, setGifs] = useState([]);
+    const [query, setQuery] = useState("dolphins");
+
+useEffect(() => {
+
+    fetch("https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=ZWSMro8wlWQqRISmJ6AJZR0hcMHiWVOS&rating=g")
+      .then((response) => response.json())
+      .then(({data}) => {
+
+        const gifs = data.map((gif) => ({url: gif.images.original.url}));
+        setGifs(gifs);
+      });
+  }, [query]);
+
+  return (  
+  <div style={{ display: "flex" }}>
+       <GifList gifs={gifs} />
+      <GifSearch onSubmitQuery={setQuery} />
+
+
+      
+  </div>
+  )
+  }
+
+
+
+
+export default GifListContainer;
